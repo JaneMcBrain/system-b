b092b3eccbee299cad8803d99a2fbf4f1c442666
Janine Semmler <syries@gmx.de>
2023-03-05 13:35:06 +0100

    fix svg console error

--- src/components/UpScroll.tsx
+++ src/components/UpScroll.tsx
@@ -3,7 +3,7 @@ export default function ScrollDown(){
     <svg className="up-scroll" xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75">
       <g id="Gruppe_37" data-name="Gruppe 37" transform="translate(-983 -808)">
         <circle id="Ellipse_1" data-name="Ellipse 1" cx="37.5" cy="37.5" r="37.5" transform="translate(983 808)" fill="#e5f851"/>
-        <text id="UP" transform="translate(997 859)" fill="none" stroke="#1a1a1a" stroke-width="2" fontSize="37" fontFamily="roc-grotesk" fontWeight="700"><tspan x="0" y="0">UP</tspan></text>
+        <text id="UP" transform="translate(997 859)" fill="none" stroke="#1a1a1a" strokeWidth="2" fontSize="37" fontFamily="roc-grotesk" fontWeight="700"><tspan x="0" y="0">UP</tspan></text>
       </g>
     </svg>
   )
