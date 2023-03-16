da3c6e818fbc5c62b757bc58e216932c1cf470dd
Janine Semmler <janine.semmler@t-systems.com>
2023-03-16 18:03:08 +0100

    add mobile navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,16 +1,34 @@
+import { useState } from 'react';
 import './styles.sass'
 interface NavigationProps{
   items: string[]
 }
 
 export default function Navigation({items}: NavigationProps){
+  const [iconClass, setIconClass] = useState<string>('nav-bar')
+  const [navClass, setNavClass] = useState<string>('navigation')
+
+  const onIconClick = () => {
+    console.log('click')
+    if(iconClass.includes('is-active')){
+      setIconClass('nav-bar');
+      setNavClass('navigation')
+    } else {
+      setIconClass('nav-bar is-active');
+      setNavClass('navigation is-active')
+    }
+  }
+
   return(
-    <ul className="navigation">
-      {items.map(item =>
-        <li key={item}>
-          <a href={`#${item.trim()}`}>{item}</a>
-        </li>
-      )}
-    </ul>
+    <div className="header">
+      <div className={iconClass} onClick={onIconClick}></div>
+      <ul className={navClass}>
+        {items.map(item =>
+          <li key={item}>
+            <a href={`#${item.trim()}`}>{item}</a>
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
